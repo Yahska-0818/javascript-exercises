8cc2d2d43e15cf10796c2b2a1e95bcb659c7100b
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-05-04 07:40:27 +0000

    Passed all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    finalString = ""
+    stringLength = stringToReverse.length
 
+    for (let i = stringLength-1; i >= 0; i--) {
+        finalString+=stringToReverse[i];
+    }
+    return finalString
 };
 
 // Do not edit below this line
