3132664c2ac64e011cb4919e52c7025906708d47
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-05-04 07:51:32 +0000

    Passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayToRemoveFrom,...args) {
+    arrayToReturn = []
+    arrayToRemoveFrom.forEach(partOfArray => {
+        if (!args.includes(partOfArray)) {
+            arrayToReturn.push(partOfArray)
+        }
+    });
+    return arrayToReturn
 };
 
 // Do not edit below this line
