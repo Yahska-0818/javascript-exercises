3a2a132cfe717d47f7ac99b0c046172905fcf52e
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-05-04 07:31:16 +0000

    Passed all 8 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat,numerOfRepeats) {
+    if (numerOfRepeats >= 0) {
+        let finalString = ""
+        for (let i=0; i<numerOfRepeats;i++) {
+            finalString+=stringToRepeat;
+        }
+        return finalString
+    }
 
+    else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
