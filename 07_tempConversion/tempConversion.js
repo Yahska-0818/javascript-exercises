133127998b8f615a7fb361c2c1593dd8db660eb9
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-05-04 08:29:37 +0000

    Passed all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  return Math.round(((tempInF-32)/1.8)*10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  return Math.round((tempInC*1.8+32)*10)/10
 };
 
 // Do not edit below this line
