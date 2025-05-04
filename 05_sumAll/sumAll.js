0845b51334ae157fc179f31eece48570d0107684
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-05-04 08:12:18 +0000

    Passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,34 @@
-const sumAll = function() {
+const sumAll = function sumAll(intOne,intTwo) {
+    if (intOne>=0 && intTwo>=0) {
+        if (Number.isInteger(intOne) && Number.isInteger(intTwo)) {
+            if (intOne%1 === 0 && intTwo%1 === 0) {
+                let finalSum = 0
+                if (intTwo > intOne) {
+                    for (let i = intOne; i < intTwo + 1; i++) {
+                        finalSum += i;
+                    }
+                    return finalSum
+                }
+                else {
+                    for (let i = intTwo; i < intOne + 1; i++) {
+                        finalSum += i;
+                    }
+                    return finalSum
+                }
+            }
+            else {
+                return "ERROR"
+            }
+        }
+        else {
+            return "ERROR"
+        }
+
+    }
+    
+    else {
+        return "ERROR"
+    }
 
 };
 
