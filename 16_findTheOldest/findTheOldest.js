f5f9b109488fda5998a349d7e608750c29c75923
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-05-11 11:57:49 +0000

    solved

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
 
+const findTheOldest = function(array) {
+    let sortedArray = array.sort(function(firstPerson,secondPerson){
+        if (firstPerson.yearOfDeath === undefined) {
+            firstPerson.yearOfDeath = 2025
+        }
+        if (secondPerson.yearOfDeath === undefined) {
+            secondPerson.yearOfDeath = 2025
+        }
+        const firstPersonAge = firstPerson.yearOfDeath - firstPerson.yearOfBirth
+        const secondPersonAge = secondPerson.yearOfDeath - secondPerson.yearOfBirth
+        if (firstPersonAge > secondPersonAge) {
+            return -1
+        }
+        else {
+            return 1
+        }
+    })
+    return sortedArray[0]
 };
 
 // Do not edit below this line
