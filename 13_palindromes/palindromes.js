f82797f1e7694c01576195ef5e364ffffc138022
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-05-11 11:07:02 +0000

    solved

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,33 @@
-const palindromes = function () {
+function isAlphaNumerical(c) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    if (alphanumerical.includes(c)) {
+        return true
+    }
+    else return false
+}
 
+const palindromes = function (string) {
+    let finalArr = []
+    let secondArr = []
+    let start = 0
+    for (let i = string.length-1; i >= 0; i--) {
+        if (isAlphaNumerical(string[i].toLowerCase())) {
+            finalArr.push(string[i].toUpperCase())
+        }
+        if (isAlphaNumerical(string[start].toLowerCase())) {
+            secondArr.push(string[start].toUpperCase())
+        } 
+        start += 1
+    }
+    for (let i = 0; i<finalArr.length;i++) {
+        if (finalArr[i]!==secondArr[i]) {
+            return false
+        }
+    }
+    return true
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
