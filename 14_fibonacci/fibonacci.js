b7d6f9abf6186ba34b85ce285279c354ccf662c3
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-05-11 11:24:09 +0000

    solved

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const zero = 0
+    const first = 1
+    let previous = zero
+    let current = first
+    let temp = 0
+
+    if (num < 0) {
+        return "OOPS"
+    }
+    else if (num == 0) {
+        return zero
+    }
+    else if (num == 1) {
+        return first
+    }
+    else {
+        for (let i = 0; i<num-2; i++) {
+            temp = current
+            current += previous
+            previous = temp
+        }
+        return previous+current
+    }
 
 };
 
